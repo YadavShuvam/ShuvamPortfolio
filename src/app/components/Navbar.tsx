254b436b3b664db460c75f7a84f8b419e12d8897
Zaddy <108516378+ZaddyAI@users.noreply.github.com>
2025-06-07 13:35:31 +0545

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div className='text-green flex justify-center'>Yo Chai logo banepaxi matra</div>
+        <div className='text-green flex justify-center'></div>
     )
 }
 
